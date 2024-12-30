4af0144a744253b2263088ccaeca5a7dfa626cc4
afshanqasim07 <afshaanqasim07>
2024-12-30 14:45:40 -0800

    done

---
+++ dart basics/index.dart
@@ -0,0 +1,18 @@
+// hello dart this is my first time writing dart code
+// hope it will be easier for me too learn
+// intro to dart
+// its a client optimized language for differnt platforms like app,web,server by google similar to js and c java its an object oriented language class based
+// this is the normal syntex od dart
+void main() {
+  print("hello Dart");
+  // DATATYPES AND VARIBALES
+// we have two variables in dart 1.var 2.final can give any name to varible var can guess any datatype and can be changeable but final we cant change again ist the constant one .
+  var name = 'afshanqasim';
+  final age = 19;
+  print(name);
+  print(age);
+  // data types
+  // 1.string = 'afshan'; 2. int = 19;  3.double = 20.26 decimalone  ;
+  // 4. boolean =true/false ; 5. 
+   // howtocheckdatatype { varname.runtimeType}
+}
